9c2a08b7b38b1482ac2413416366f52eee2dc883
alexvest <alexander.vestin@kau.se>
2023-08-31 15:18:14 +0200

    Slutkod F3

--- Hantera_data_v1/app/src/main/java/com/example/hantera_data_v1/MainActivity.java
+++ Hantera_data_v1/app/src/main/java/com/example/hantera_data_v1/MainActivity.java
@@ -3,13 +3,42 @@ package com.example.hantera_data_v1;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
-import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+
+    private EditText idET, nameET, pnrET;
+    private Button addBtn;
+    private TextView studentList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        new Database();
+        idET = findViewById(R.id.student_id);
+        nameET = findViewById(R.id.student_namn);
+        pnrET = findViewById(R.id.student_pnr);
+        addBtn = findViewById(R.id.add_btn);
+        studentList = findViewById(R.id.student_list);
+
+        addBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                int id = Integer.parseInt(idET.getText().toString());
+                String namn = nameET.getText().toString();
+                String pnr = pnrET.getText().toString();
+                Student s = new Student(id, namn, pnr);
+
+                Database.instance.add(s);
+
+                studentList.setText(Database.instance.printStudents());
+            }
+        });
+
     }
 }
